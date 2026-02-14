154d36724dafb90b2e8e03b2a872a3c3fd420ac2
enis nishori <en46627@ubt-uni.net>
2026-02-14 09:52:40 +0100

    user service updated

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs'); // Added bcrypt import
+const bcrypt = require('bcryptjs');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -20,14 +20,13 @@ exports.registerUser = async (userData) => {
         throw new Error('User already exists');
     }
 
-    // --- LOGIC MOVED HERE ---
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({
         fullname,
         email,
-        password: hashedPassword // Save the hashed version
+        password: hashedPassword
     });
 
     const token = generateToken(user._id);
@@ -41,14 +40,11 @@ exports.loginUser = async (loginData) => {
         throw new Error('Please provide email and password');
     }
 
-    // Select password because 'select: false' is in the schema
     const user = await User.findOne({ email }).select('+password');
     if (!user) {
         throw new Error('Invalid credentials');
     }
 
-    // --- LOGIC MOVED HERE ---
-    // Instead of user.matchPassword, we use bcrypt directly
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         throw new Error('Invalid credentials');
@@ -56,7 +52,6 @@ exports.loginUser = async (loginData) => {
 
     const token = generateToken(user._id);
 
-    // Remove password from the user object before returning to controller
     user.password = undefined;
 
     return { user, token };
